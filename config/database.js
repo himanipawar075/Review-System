befb654b85dbcd21947b70ccd16c6f0162999cd8
Himani Narendra Pawar <144578396+himanipawar075@users.noreply.github.com>
2024-11-23 22:51:56 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,17 +1,10 @@
-import mongoose from 'mongoose';
-
-const dbConnect = () => {
-  mongoose
-    .connect("mongodb+srv://himani:himani123@cluster0.rrn1k.mongodb.net/FeedbackSystem", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => console.log("DB connected successfully"))
-    .catch((error) => {
-      console.log("DB connection failed");
-      console.error(error);
-      process.exit(1); // Exit the process on DB connection failure
-    });
-};
-
+import mongoose from "mongoose";
+const dbConnect=async()=>{
+    try {
+       await mongoose.connect("mongodb+srv://himani:himani123@cluster0.rrn1k.mongodb.net/FeedbackSystem")
+       console.log("MongoDB connected successfully")
+    } catch (error) {
+        console.log("Connection failed",error)
+    }
+}
 export default dbConnect;
