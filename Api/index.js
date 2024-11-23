748770cc3df5d531e4fa6e039ddb51395db1bd8a
Himani Narendra Pawar <144578396+himanipawar075@users.noreply.github.com>
2024-11-23 23:28:31 +0530

    Update index.js

--- Api/index.js
+++ Api/index.js
@@ -17,7 +17,7 @@ app.get("/",(req,res)=>{
 res.send("hello");
 })
 
-const PORT=process.env.PORT||3000;
+const PORT=process.env.PORT||4000;
 app.listen(PORT,()=>{
  console.log(`Server is running at port: ${PORT}`);
 })
